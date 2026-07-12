374c2ce75e92c7c505628d7c7a7306d5a18ce827
FrancescoMonti-source <58150610+FrancescoMonti-source@users.noreply.github.com>
2026-07-12 22:53:17 +0200

    Retire prototype test suite

--- tests/testthat.R
+++ tests/testthat.R
@@ -1,4 +1,12 @@
 library(testthat)
 library(extractionengine)
 
-test_check("extractionengine")
+test_files <- list.files(
+  "testthat",
+  pattern = "^test.*\\.[Rr]$",
+  full.names = TRUE
+)
+
+if (length(test_files)) {
+  test_check("extractionengine")
+}
